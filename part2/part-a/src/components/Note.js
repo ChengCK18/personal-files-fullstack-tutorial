819d310faf0215f36587bbe48768856281a6d16c
ChengCK18 <43441027+ChengCK18@users.noreply.github.com>
2021-06-19 22:04:05 +0800

    Part 2: Section B and Exercise 2.6 to 2.10

--- part2/part-a/src/components/Note.js
+++ part2/part-a/src/components/Note.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Note = ({id,note}) => {
     return(
-    <li>{note.content}</li>
+    <div>
+        <li>{note.content}</li> {note.important ? "Important": "Not important"}
+        
+    </div>
     )
 }
 
