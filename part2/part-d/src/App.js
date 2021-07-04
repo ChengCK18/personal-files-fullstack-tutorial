ac4a9b13ab6733b98d6d9ec233d1c2efa697bbdb
ChengCK18 <43441027+ChengCK18@users.noreply.github.com>
2021-07-04 16:35:27 +0800

    Part2: Amended package.json

---
+++ part2/part-d/src/App.js
@@ -0,0 +1,63 @@
+import axios from 'axios'
+import React,{useEffect, useState} from 'react'
+import Note from './components/Note'
+
+const App = (props) => {
+  const [notes, setNotes] = useState([])
+  const [newNote,setNewNote] = useState(' ')
+  const [showAll,setShowAll] = useState(true)
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+
+  useEffect(()=>{
+    console.log("Da effect")
+    axios
+      .get('http://localhost:3001/notes')
+      .then( response => {
+        console.log("Promise fuffiled :D")
+        setNotes(response.data)
+      })
+  },[])
+  console.log('render', notes.length, 'notes')
+
+  const addNote = (event) =>{
+    event.preventDefault()
+    const noteObject = {
+      content:newNote,
+      date:new Date().toISOString(),
+      important:Math.random() <0.5,
+      id:notes.length +1
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) =>{ //since App component controls the behaviour of input element due to assingment of App component state
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick ={() => setShowAll(!showAll)}>
+          Show {showAll ? 'important':'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input onChange={handleNoteChange} value={newNote}/>
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  )
+}
+
+
+export default App
