c8c3c449ba5c46a0daf78acb997617f6233b3fa6
ChengCK18 <43441027+ChengCK18@users.noreply.github.com>
2022-03-17 21:42:10 +0800

    React routing

--- 2_django/django-react/music_controller/frontend/src/components/App.js
+++ 2_django/django-react/music_controller/frontend/src/components/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class App extends Component {
     constructor(props) {
         super(props);
+
     }
 
     render() {
-        return (<h1>Testing React Code</h1>)
+        return (
+            <div>
+                {this.props.name}
+                <HomePage />
+            </div>)
     }
 }
 
 const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+render(<App name="Wilson" />, appDiv);
