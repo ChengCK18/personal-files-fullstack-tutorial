95f1c5493378a5d4d178dbde3c690dc39c1c9b16
ChengCK18 <chinkheng_98@hotmail.com>
2022-08-08 23:26:49 +0800

    Exercise 5.16

--- 1_fullstackopen/part5/ex-5-13-to-5-16/src/components/BlogForm.test.js
+++ 1_fullstackopen/part5/ex-5-13-to-5-16/src/components/BlogForm.test.js
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import BlogForm from './BlogForm'
+//import { shallow } from 'enzyme'
 
 test('<BlogForm /> updates parent state and calls onSubmit', async () => {
 
@@ -16,7 +17,7 @@ test('<BlogForm /> updates parent state and calls onSubmit', async () => {
 
 
 
-    const { container } = render(
+    render(
         <BlogForm
             blogTitle={''}
             setBlogTitle={setBlogTitle}
@@ -29,20 +30,38 @@ test('<BlogForm /> updates parent state and calls onSubmit', async () => {
         />)
 
 
-    const inputTitle = screen.getByPlaceholderText('input blog title here')
-    const inputAuthor = screen.getByPlaceholderText('input blog author here')
-    const inputUrl = screen.getByPlaceholderText('input blog url here')
+    let inputTitle = screen.getByPlaceholderText('input blog title here')
+    let inputAuthor = screen.getByPlaceholderText('input blog author here')
+    let inputUrl = screen.getByPlaceholderText('input blog url here')
 
-    await userEv.type(inputTitle, 'the_title')
-    await userEv.type(inputAuthor, 'the_author')
-    await userEv.type(inputUrl, 'the_url')
+
+    // await userEv.type(inputTitle, 'the_title')
+    // await userEv.type(inputAuthor, 'the_author')
+    // await userEv.type(inputUrl, 'the_url')
     //expect(inputTitle).toHaveValue('the_title')
 
-
     const createButton = screen.getByText('Create')
-
     await userEv.click(createButton)
 
     expect(handleCreateBlog.mock.calls).toHaveLength(1)
+    expect(inputTitle).toBeDefined()
+    expect(inputAuthor).toBeDefined()
+    expect(inputUrl).toBeDefined()
+
+
+    // const loginComponent = shallow(<BlogForm
+    //     blogTitle={''}
+    //     setBlogTitle={setBlogTitle}
+    //     blogAuthor={''}
+    //     setBlogAuthor={setBlogAuthor}
+    //     blogUrl={''}
+    //     setBlogUrl={setBlogUrl}
+    //     handleCreateBlog={handleCreateBlog}
+    //     handleCancelBlog={handleCancelBlog}
+    // />)
+    // loginComponent.setState({ blogTitle: 'blyat' })
+    // screen.debug(loginComponent)
+    //expect(loginComponent.find(Notification).length).toBe(1)
+
     //Input based on state, need to test as well
 })
