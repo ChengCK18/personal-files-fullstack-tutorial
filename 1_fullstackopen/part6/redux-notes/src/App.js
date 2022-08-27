864b1233c0b4a095c001f557aa8515dc7bd93762
ChengCK18 <chinkheng_98@hotmail.com>
2022-08-27 22:14:27 +0800

    Part 6, C notes

--- 1_fullstackopen/part6/redux-notes/src/App.js
+++ 1_fullstackopen/part6/redux-notes/src/App.js
@@ -1,15 +1,26 @@
+import { useEffect } from 'react'
+
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
+import noteService from './services/notes'
+import { setNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
+
 
 const App = () => {
-  return (
-    <div>
-      <NewNote />
-      <VisibilityFilter />
-      <Notes  />
-    </div>
-  )
+    const dispatch = useDispatch()
+    useEffect(() => {
+        noteService.getAll().then(notes => dispatch(setNotes(notes)))
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+    return (
+        <div>
+            <NewNote />
+            <VisibilityFilter />
+            <Notes />
+        </div>
+    )
 }
 
 export default App
--- 1_fullstackopen/part6/redux-notes/src/index.js
+++ 1_fullstackopen/part6/redux-notes/src/index.js
@@ -3,18 +3,26 @@ import ReactDOM from 'react-dom/client'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
-import noteReducer from './reducers/noteReducer'
+import noteReducer, { setNotes } from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
+import noteService from './services/notes'
+
 
 const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
+    reducer: {
+        notes: noteReducer,
+        filter: filterReducer
+    }
 })
 
+noteService.getAll().then(notes => {
+    store.dispatch(setNotes(notes))
+})
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
