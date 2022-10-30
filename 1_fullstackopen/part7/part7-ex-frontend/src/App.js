6285d6748d803994b808325c2068a79a05e30970
ChengCK18 <chinkheng_98@hotmail.com>
2022-10-30 16:43:32 +0800

    Part 7 complete with bootstrap styling

--- 1_fullstackopen/part7/part7-ex-frontend/src/App.js
+++ 1_fullstackopen/part7/part7-ex-frontend/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     });
 
     return (
-        <div className="container">
+        <div style = {{height:"100vh"}} className="container">
             {user === null && <LoginPanel />}
             {user !== null && <HomePanel />}
            
