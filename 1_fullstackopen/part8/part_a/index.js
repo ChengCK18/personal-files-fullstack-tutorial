a8d961abd7077a9a8a66d02a3ec292d0a9440633
ChengCK18 <chinkheng_98@hotmail.com>
2023-03-11 16:17:55 +0800

    Completed exercise 8.1 to 8.3

--- 1_fullstackopen/part8/part_a/index.js
+++ 1_fullstackopen/part8/part_a/index.js
@@ -1,7 +1,6 @@
-const { ApolloServer, gql } = require('apollo-server')
-const { v1: uuid } = require('uuid')
-const { GraphQLError } = require('graphql')
-
+const { ApolloServer, gql } = require("apollo-server");
+const { v1: uuid } = require("uuid");
+const { GraphQLError } = require("graphql");
 
 let persons = [
     {
@@ -9,65 +8,60 @@ let persons = [
         phone: "040-123543",
         street: "Tapiolankatu 5 A",
         city: "Espoo",
-        id: "3d594650-3436-11e9-bc57-8b80ba54c431"
+        id: "3d594650-3436-11e9-bc57-8b80ba54c431",
     },
+
     {
         name: "Matti Luukkainen",
         phone: "040-432342",
         street: "Malminkaari 10 A",
         city: "Helsinki",
-        id: '3d599470-3436-11e9-bc57-8b80ba54c431'
+        id: "3d599470-3436-11e9-bc57-8b80ba54c431",
     },
     {
         name: "Venla Ruuska",
         street: "Nallemäentie 22 C",
         city: "Helsinki",
-        id: '3d599471-3436-11e9-bc57-8b80ba54c431'
+        id: "3d599471-3436-11e9-bc57-8b80ba54c431",
     },
-]
-
+];
 
 // Defining the GraphQL schema here ;D
 // Includes a 'Query' type to outline what can be queried
 const typeDefs = gql`
-
     enum YesNo {
         YES
         NO
     }
 
-    type Address{
-        street:String!
-        city:String!
+    type Address {
+        street: String!
+        city: String!
     }
 
-    type Person{
-        name:String!
-        phone:String
-        address:Address!
-        id:ID!
+    type Person {
+        name: String!
+        phone: String
+        address: Address!
+        id: ID!
     }
 
-    type Query{
-        personCount:Int!
-        allPersons(phone:YesNo):[Person!]!
-        findPerson(name:String!):Person
-
+    type Query {
+        personCount: Int!
+        allPersons(phone: YesNo): [Person!]!
+        findPerson(name: String!): Person
     }
-    type Mutation{
+    type Mutation {
         addPerson(
-            name:String!
-            phone:String
-            street:String!
-            city:String!
-        ):Person
+            name: String!
+            phone: String
+            street: String!
+            city: String!
+        ): Person
 
-        editNumber(
-            name:String!
-            phone:String!
-        ):Person
+        editNumber(name: String!, phone: String!): Person
     }
-`
+`;
 
 //Based on the define 'Query', how to resolve for each Query
 // findPerson's 'root' will be 'parent' if there are previous chain resolver
@@ -78,59 +72,63 @@ const resolvers = {
     Query: {
         personCount: () => persons.length,
         allPersons: (root, args) => {
-            if (!args.phone) { //ENUM args can be null
-                return persons
+            if (!args.phone) {
+                //ENUM args can be null
+                return persons;
             }
 
-            const byPhone = (person) => {
-                args.phone === 'YES' ? person.phone : !person.phone
-            }
+            const byPhone = (pers) => {
+                return args.phone === "YES"
+                    ? "phone" in pers
+                    : !("phone" in pers);
+            };
 
-
-            return persons.filter(byPhone)
+            return persons.filter(byPhone);
         },
-        findPerson: (root, args) => persons.find(p => p.name === args.name)
+        findPerson: (root, args) => persons.find((p) => p.name === args.name),
     },
     Person: {
-        address: (root) => {//root in this case refers to Person original object
+        address: (root) => {
+            //root in this case refers to Person original object
             return {
                 street: root.street,
-                city: root.city
-            }
-        }
+                city: root.city,
+            };
+        },
     },
     Mutation: {
         addPerson: (root, args) => {
-
-            if (persons.find(p => p.name === args.name)) {
-                throw new GraphQLError('Name must be unique', {
+            if (persons.find((p) => p.name === args.name)) {
+                throw new GraphQLError("Name must be unique", {
                     extensions: {
-                        code: 'BAD_USER_INPUT',
-                        invalidArgs: args.name
-                    }
-                })
+                        code: "BAD_USER_INPUT",
+                        invalidArgs: args.name,
+                    },
+                });
             }
 
-            const person = { ...args, id: uuid() }
-            persons = persons.concat(person)
-            return person
+            const person = { ...args, id: uuid() };
+            persons = persons.concat(person);
+            return person;
         },
         editNumber: (root, args) => {
-            const person = persons.find(p => p.name === args.name)
-            if (!person) { //No one found with given name
-                return null
+            const person = persons.find((p) => p.name === args.name);
+            if (!person) {
+                //No one found with given name
+                return null;
             }
-            const updatedPerson = { ...person, phone: args.phone } //spread operator
-            persons = persons.map(p => p.name === args.name ? updatedPerson : p)
-            return updatedPerson
-        }
-    }
+            const updatedPerson = { ...person, phone: args.phone }; //spread operator
+            persons = persons.map((p) =>
+                p.name === args.name ? updatedPerson : p
+            );
+            return updatedPerson;
+        },
+    },
     // Person: { //overwriting default resolver which returns orignal property of the obj
     //     street: (root) => "Manhattan", //everyone is from manhattan new york D;
     //     city: (root) => "New York"
     // }
-
-}
+};
 
 //Apollo server object requires two parameters
 // 1, GraphQL Schema
@@ -138,10 +136,8 @@ const resolvers = {
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-})
+});
 
 server.listen().then(({ url }) => {
-    console.log(`Server ready at ${url}`)
-})
-
-
+    console.log(`Server ready at ${url}`);
+});
